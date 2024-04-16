9f7d095d89118c0b5d93d4dd806edc8c010171f8
sunsetheus <williamsalimrjs@gmail.com>
2024-04-16 21:16:38 +0700

    feat: add review model

---
+++ src/main/java/id/ac/ui/cs/advprog/review/model/Review.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.review.model;
+
+public class Review {
+    private String reviewId;
+    private String boxId;
+    private int userId;
+    private int rating;
+    private String reviewText;
+
+}
