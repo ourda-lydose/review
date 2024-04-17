df48676e9709ddb1d840d3eeaef31ddf043c6ab4
sunsetheus <williamsalimrjs@gmail.com>
2024-04-17 15:52:09 +0700

    refactor: comment test and implementation for repository

---
+++ src/main/java/id/ac/ui/cs/advprog/review/repository/ReviewRepository.java
@@ -0,0 +1,13 @@
+//package id.ac.ui.cs.advprog.review.repository;
+//
+//import id.ac.ui.cs.advprog.review.model.Review;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Optional;
+//
+//@Repository
+//public interface ReviewRepository extends CrudRepository<Review, Integer> {
+//    Optional<Review> findReviewByReviewId(String reviewId);
+//    Optional<Review> findReviewByUserIdAndBoxId(int userId, String boxId);
+//}
